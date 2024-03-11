9d433b245659a961924470c9a744cd17fa33b385
Artem Matok <artemmatok@gmail.com>
2024-03-11 23:38:01 +0200

    Update ChatService

--- DemoChat/DemoChat.Client/ChatServices/ChatService.cs
+++ DemoChat/DemoChat.Client/ChatServices/ChatService.cs
@@ -19,7 +19,7 @@ namespace DemoChat.Client.ChatServices
 
         public void ReceiveMessage()
         {
-            _hubConnection.On<Chat>("ReceiveMessage", (chat) =>
+            _hubConnection.On<Chat>("RecieveMessage", (chat) =>
             {
                 Chats.Add(chat);
                 InvokeChatDisplay?.Invoke();
